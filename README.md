1aaed0ede4605a810904c14c80fdcd4b9c53040d
Juliano <juliano.co.lopes@gmail.com>
2024-05-15 00:06:22 -0300

    Desafio concluído

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# sistema-bancario-dio
+# Sistema Bancario DIO
 Desafio DIO para criação de um sistema bancário utilizando Python
+
+---
+## Requisitos
+
+### Operações
+---
+---
+1. Depósito
+
+#### Detalhe sobre operação depósito
+* [x] Ser possível depositar _**valores positivos**_ na conta bancária
+* [x] Depósitos devem ser armazenados em uma **variável** e exibidos na **Operação Extrato** _[3]_
+---
+
+2. Saque
+
+#### Detalhe sobre operação saque
+* [x] Permitir _3 saques diários_ com **limite máximo** de _R$ 500,00_
+* [x] Exibir **mensagem de falta de saldo** se não tiver saldo em conta
+* [x] Saques devem ser armazenados em uma **variável** e exibidos na **Operação Extrato** _[3]_
+---
+3. Extrato
+
+#### Detalhe sobre operação extrato
+* [x] Listar **TODOS** _depósitos_ e _saques_
+* [x] Exibir **saldo atual** no _fim da listagem_
+* [x] Exibir mensagem em branco caso não haja movimentações
+---
+
+### Formato dos valores
+* [x] R$ #.##
+    * Exemplo: R$ 20.50
+---
+
+## Extras
+
+* Limpa a tela ao iniciar a aplicação e a cada operação bem sucedida
+* Validação de inputs do usuário, não permitindo ele inserir um ```> ENTER``` ou uma string vazia, que apresentará um ValueError, para isso, utilizamos _**try**_ | _**except**_ para conferir se o input será válido.
---
+++ sistema_bancario.py
@@ -0,0 +1,127 @@
+from os import system
+import platform
+
+if platform.system() == "Windows":
+    system("cls")
+else:
+    system("clear")
+
+header = f"""
+    {"=" * 50}
+    {"sistema bancário dio".upper().center(50, " ")}
+    {"=" * 50}
+
+"""
+print(header)
+
+nome_usuario = input("Nome do usuário: ")
+
+print(f"Seja bem-vindo(a) {nome_usuario.title()} ao {'sistema bancário dio'.upper()}!!!")
+
+# Variáveis
+
+deposito = f"""
+{"-" * 30}
+"""
+saque = f"""
+{"-" * 30}
+"""
+extrato = f"""
+{'extrato'.upper()}
+{"-" * 30}
+"""
+saldo = 0
+saque_acumulado = 0
+limite_saque = 500
+quantidade_saques = 0
+
+while True:
+    
+    menu = """
+    Qual operação deseja fazer?
+    
+    [1] Depósito
+    [2] Saque
+    [3] Extrato
+    [4] Sair
+    """
+
+    print(menu)
+    try:
+        opcao = int(input("\tDigite sua opção: "))
+    except ValueError:
+        print(f"\nO valor digitado não é válido.\n")
+        continue
+    print()
+
+
+    # Opção inválida
+    if opcao not in range(1, 5):
+        print("\nVocê digitou uma opção inválida!")
+        continue
+
+    # Opção de depósito
+    elif opcao == 1:
+        valor_depositado = float(input("Valor a ser depositado: "))
+        if valor_depositado <= 0:
+            print("Depósitos devem ser maiores que 0(zero)")
+            continue
+        saldo += valor_depositado
+        entrada = f"""\rDepósito de R$ {valor_depositado:.2f}
+        \r{"-" * 30}
+        """
+        deposito += entrada
+        extrato += entrada
+
+    # Opção de saque
+    elif opcao == 2:
+        if saldo <= 0:
+            print("Você não tem saldo em conta para realizar operação de saque!")
+            continue
+
+        if quantidade_saques >= 3:
+            print(f"{'-' * 30}\nVocê excedeu a quantidade diária permitida para realizar saques.\n{'-' * 30}")
+            continue
+
+        valor_a_sacar = float(input("Valor a ser sacado: "))
+
+        if saque_acumulado + valor_a_sacar > limite_saque or valor_a_sacar > limite_saque:
+            print(f"{'-' * 30}\nO limite diário para saque é de R$ {limite_saque:.2f}\n{'-' * 30}")
+            continue
+
+        if valor_a_sacar > saldo:
+            print(f"O valor do saque de R$ {valor_a_sacar:.2f} excede seu saldo de R$ {saldo:.2f}")
+            continue
+
+        saldo -= valor_a_sacar
+        saque_acumulado += valor_a_sacar
+        quantidade_saques += 1
+        saida = f"""\rSaque de R$ {valor_a_sacar:.2f}
+        \r{"-" * 30}
+        """
+        saque += saida
+        extrato += saida
+
+    elif opcao == 3:
+        print(header)
+        if platform.system() == "Windows":
+            system("cls")
+        else:
+            system("clear")
+        print(extrato)
+        if extrato.find("Depósito") == -1 and extrato.find("Saque") == -1:
+            extrato += "Sem movimentação na conta"
+        continue
+
+    elif opcao == 4:
+        print("\nObrigado por utilizar nossos serviços!!!")
+        break
+
+    if platform.system() == "Windows":
+        system("cls")
+    else:
+        system("clear")
+
+    print(header)
+    
+    print(f"{'-' * 30}\nSaldo atual: R$ {saldo:.2f}\n{'-' * 30}")
